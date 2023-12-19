c16517cd7920dfcc05347c099f455a99122ce670
Pinapelz <yukais@pinapelz.com>
2023-12-18 19:34:39 -0800

    feat: add support for vorbis

--- api/index.py
+++ api/index.py
@@ -9,7 +9,8 @@ from tempfile import NamedTemporaryFile
 import redis
 from dotenv import load_dotenv
 import json
-
+import mutagen.flac
+from enum import Enum
 
 app = Flask(__name__)
 CORS(app)
@@ -25,6 +26,13 @@ REDIS_PORT = os.environ.get("REDIS_PORT")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
 ENABLE_CACHE = os.environ.get("ENABLE_CACHE") == "True"
 CACHE_PLAYLISTS= os.environ.get("CACHE_PLAYLISTS") == "True"
+
+
+class TagType(Enum):
+    ID3 = 1
+    VORBIS = 2
+    NONE = 99
+
 
 
 class RedisCache:
@@ -64,50 +72,27 @@ def validate_image_mime(image_data: bytes, expected_mime: str) -> bool:
             result = True
     return result
 
-def get_mp3_id3v2_tags(url: str, chunk_size=DEFAULT_CHUNK_SIZE) -> bool:
-    """
-    Check if the remote mp3 file if ID3v2
-    """
-    response = requests.get(url, stream=True)
-    if response.status_code != 200:
-        print("Error downloading the file")
-        raise InvalidFileError("Error downloading the file. Remote server responded with status code " + str(response.status_code))
-    verified_is_ID3_V2 = False
-    with NamedTemporaryFile(delete=False) as temp_file:
-        bytes_written = 0
-        for chunk in response.iter_content(chunk_size=chunk_size):
-            temp_file.write(chunk)
-            temp_file.flush()
-            bytes_written += len(chunk)
-            # Check that the file starts with 'ID3' meaning its ID3v2
-            if not verified_is_ID3_V2:
-                with open(temp_file.name, 'rb') as f:
-                    if f.read(3) == b'ID3':
-                        verified_is_ID3_V2 = True
-                    else:
-                        raise InvalidFileError("[ERROR] The file was not recognized as ID3v2")
-            audio_file = eyed3.load(temp_file.name)
-            if audio_file is not None and audio_file.tag is not None:
-                print(f"[INFO] ID3 tag found for file: {url}")
-                if audio_file.tag.images:
-                    if validate_image_mime(audio_file.tag.images[0].image_data, audio_file.tag.images[0].mime_type):
-                        print("[INFO] Image data found and validated")
-                        break
-                    else:
-                        print("[INFO] Image data found but failed validation. Continuing to download the file")
-                else:
-                    print("[INFO] No image data found. ID3 tag successful breaking out of loop")
-                    break
-            print(f"[INFO] Chunk {bytes_written} bytes written to disk but failed to find ID3 tag")
 
+def get_id3_tag(filepath: str):
+    audio_file = eyed3.load(filepath)
+    if audio_file.tag is None or audio_file.tag is None:
+        return None
+    image_data = None
+    if audio_file.tag.images:
+        if validate_image_mime(audio_file.tag.images[0].image_data, audio_file.tag.images[0].mime_type):
+            print("[INFO] Image data found and validated")
+        else:
+            print("[INFO] Image data found but failed validation. Continuing to download the file")
+            return None
+    else:
+        print("[INFO] No image data found. Tags successful breaking out of loop")
     if audio_file is None:
-        raise InvalidFileError("[ERROR] The file was not recognized as ID3v2")
+        raise InvalidFileError("[ERROR] The file was not recognized as a supported audio file")
     tag_data = {
         "album": str(audio_file.tag.album),
         "artist": str(audio_file.tag.artist),
         "title": str(audio_file.tag.title),
         "year": str(audio_file.tag.getBestDate()),
-        "url": url
     }
     if audio_file.tag.images:
         image_data = audio_file.tag.images[0].image_data
@@ -116,7 +101,73 @@ def get_mp3_id3v2_tags(url: str, chunk_size=DEFAULT_CHUNK_SIZE) -> bool:
     else:
         tag_data["album_art"] = None
     return tag_data
-        
+
+
+def get_vorbis_tag(filepath: str):
+    try:
+        audio_file = mutagen.flac.FLAC(filepath)
+        if not audio_file:
+            return None
+        tag_data = {
+            "album": audio_file.get('album')[0] if 'album' in audio_file else None,
+            "artist": audio_file.get('artist')[0] if 'artist' in audio_file else None,
+            "title": audio_file.get('title')[0] if 'title' in audio_file else None,
+            "year": audio_file.get('date')[0] if 'date' in audio_file else None,
+        }
+        if audio_file.pictures:
+            mime_type = audio_file.pictures[0].mime
+            if validate_image_mime(audio_file.pictures[0].data, mime_type):
+                print("[Info] Image data found and validated")
+                print("[Info] Mime type: " + mime_type)
+                image_data = audio_file.pictures[0].data
+                base64_image = base64.b64encode(image_data).decode('utf-8')
+                tag_data["album_art"] = base64_image
+            else:
+                print("[Info] Image data found but failed validation. Continuing to download the file")
+                return None
+        return tag_data
+    except Exception:
+        print("[Info] Failed to read file as FLAC")
+        return None
+
+def get_audio_tags(url: str, chunk_size=DEFAULT_CHUNK_SIZE):
+    """
+    Check if the remote mp3 file if ID3v2
+    """
+    response = requests.get(url, stream=True)
+    if response.status_code != 200:
+        print("Error downloading the file")
+        raise InvalidFileError("Error downloading the file. Remote server responded with status code " + str(response.status_code))
+    with NamedTemporaryFile(delete=False) as temp_file:
+        bytes_written = 0
+        meta_tag_type = TagType.NONE
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            temp_file.write(chunk)
+            temp_file.flush()
+            bytes_written += len(chunk)
+            if meta_tag_type == TagType.NONE:
+                with open(temp_file.name, 'rb') as f:
+                    first_four_bytes = f.read(4)
+                    if first_four_bytes[:3] == b'ID3':
+                        meta_tag_type = TagType.ID3
+                    elif first_four_bytes == b'fLaC':
+                        meta_tag_type = TagType.VORBIS
+                    else:
+                        raise InvalidFileError("[ERROR] Did not match any supported audio file formats")
+
+            if meta_tag_type == TagType.ID3:
+                print("[Info] Attempting to read ID3 tag of " + url)
+                tag_data = get_id3_tag(temp_file.name)
+                if tag_data:
+                    break
+            elif meta_tag_type == TagType.VORBIS:
+                print("[Info] Attempting to read Vorbis tag of " + url)
+                tag_data = get_vorbis_tag(temp_file.name)
+                if tag_data:
+                    break
+    tag_data["url"] = url
+    return tag_data
+
 
 @app.route('/api/get_playlist_data')
 def get_playlist_data():
@@ -143,7 +194,7 @@ def get_playlist_data():
                 print(f"[Info] Cache Hit for individual song {line}! Returning cached data")
                 playlist_data.append(json.loads(redis.get_data(line)))
                 continue
-            playlist_data.append(get_mp3_id3v2_tags(line))
+            playlist_data.append(get_audio_tags(line))
             if ENABLE_CACHE:
                 print(f"[Info] Cache Miss for individual song {line}! Setting cache data")
                 redis.set_data(line, json.dumps(playlist_data[-1]))
